912bc7840b525333d018ea02e721d3e4f5c6aa47
Ali <alimovmuhammadali01222008@gmail.com>
2025-11-20 17:13:25 +0500

    cmake test / tree left/right rotation

--- include/Tree.hpp
+++ include/Tree.hpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
 using namespace std;
-
-
 class Node
 {
 public:
@@ -43,7 +41,32 @@ public:
         }
         return getHeight(n->left)-getHeight(n->right);
     }
+
+    Node* rightR(Node* y){
+        Node* x = y->left;
+        Node* T2 = x->right;
+
+        x->right = y;
+        y->left = T2;
+
+        y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+        x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+
+        return x;
+    }
+
+    Node* leftR(Node* x) {
+        Node* y = x->right;
+        Node* T2 = y->left;
+
+        y->left = x;
+        x->right = T2;
+
+        x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+        y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+        return y;
+    }
+
     
-
-
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
 #include <nlohmann/json.hpp>
 #include <iostream>
+using namespace std;
 using json = nlohmann::json;
+
+int main()
+{
+    json user={
+        {"name","ali"},
+        {"database","mysql"},
+        {"skills",{"cpp","python","java"}}
+    };
+
+    cout<<user.dump(4);
+}
 
